984cc086afe2c0e2497f0a7c0fba47f12bf0d67e
Oluwafemi <oluwafemiomolounnu@gmail.com>
2024-01-02 23:09:52 +0100

    Completed sidebar

--- src/App.js
+++ src/App.js
@@ -38,5 +38,23 @@ function FriendList() {
 }
 
 function Friend({friend}) {
-  return <li>{friend.name}</li> 
+  return <li>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+
+    {friend.balance < 0 && (
+      <p className="red"> You owe {friend.name} ${friend.balance}
+      </p>
+    )}
+    
+    {friend.balance > 0 && (
+      <p className="green"> {friend.name} owes you ${friend.balance}
+      </p>
+    )}
+    
+    {friend.balance === 0 && (
+      <p> You and {friend.name} are even </p>
+    )}
+
+  </li> 
 }
